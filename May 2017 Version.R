9a5561b1e0be57c7437f75da86b22bd4dd4b5d70
McAnswer19 <45974199+McAnswer19@users.noreply.github.com>
2019-06-01 20:56:12 +0100

    Update May 2017 Version.R
    Cleaned up the code presentation and added more comments.

--- May 2017 Version.R
+++ May 2017 Version.R
@@ -1,8 +1,11 @@
-# this is a testbed for testing hypotheses relating to which alogrithm is best for updates. 
+# Simple demonstration of the algorithm. Very crude code, with issues still to be worked out. 
 
 require(HDInterval)
 
-reflection <- function(x){    # this one is obvious... 
+
+# Returns the inverse of a given scalar or vector on the interval 0-1. 
+# This is necessary for reversing the credibility region endpoints. 
+reflection <- function(x){    
   
   x <- (1 - x)
 
@@ -17,117 +20,59 @@ reflection <- function(x){    # this one is obvious...
 }
 
 
-key <- 0.95**(1/3)
-x <- c(0:100000)/100000       
+# Some variables for the call to HDI. 
+HDI_width_per_cell <- 0.95**(1/3)
+granularity <- c(0:100000)/100000       
 
-objective_a <- 6
-objective_b <- 6
+
+# The objective counts for each outcome type. i.e. what was actually observed. 
+objective_a <- 44
+objective_b <- 89
 objective_c <- 6 
-objective_d  <- 6
+objective_d  <- 7
 
 objective_ab <- objective_a + objective_b
 objective_cd <- objective_c + objective_d
 
+
+# The modifiers for each level of the tree. See the pdf for a more thorough explanation. 
 bottom_modifier <- 0.5
 top_modifier <- 1         
 
+
+# Vectors to hold the final 95% CR's for each variable. 
 overall_a <- 0
 overall_b <- 0 
 overall_c <- 0 
 overall_d <- 0 
 
-# Heads/The RIGHT SIDE is shape 1!  ___________________________________________________________________________________________________________________________
+# Heads/the right side is shape 1!  
 
 
- bottom <- hdi(qbeta(p = x,  shape1 = 2 + (objective_cd * bottom_modifier), shape2 = 2 + (objective_ab * bottom_modifier), ncp = 0, lower.tail = TRUE, log.p = FALSE), 
-              credMass = key, tol = 1e-5, allowSplit = TRUE ) 
+ bottom <- hdi(qbeta(p = granularity,  shape1 = 2 + (objective_cd * bottom_modifier), shape2 = 2 + (objective_ab * bottom_modifier), ncp = 0,
+                     lower.tail = TRUE, log.p = FALSE), credMass = HDI_width_per_cell, tol = 1e-5, allowSplit = TRUE ) 
 
-  top_left <- hdi(qbeta(p = x,  shape1 = 1 + (objective_b * top_modifier), shape2 = 1 +(objective_a * top_modifier), ncp = 0, lower.tail = TRUE, log.p = FALSE), 
-                credMass = key, tol = 1e-5, allowSplit = TRUE ) # A B 
+  top_left <- hdi(qbeta(p = granularity,  shape1 = 1 + (objective_b * top_modifier), shape2 = 1 +(objective_a * top_modifier), ncp = 0, 
+                        lower.tail = TRUE, log.p = FALSE), credMass = HDI_width_per_cell, tol = 1e-5, allowSplit = TRUE ) # A B 
 
-  top_right <- hdi(qbeta(p = x ,  shape1 = 1 + (objective_d * top_modifier), shape2 = 1 + (objective_c * top_modifier ), ncp = 0, lower.tail = TRUE, log.p = FALSE), 
-                 credMass = key, tol = 1e-5, allowSplit = TRUE ) # C D 
-  
-  
-  
-  # equal tailed intervals version. certainty = 0.98 cubed. Because I'm lazy. 
- 
-#  bottom <- qbeta(p = c(0.01, 0.99),  shape1 = 1 + (objective_cd * bottom_modifier), shape2 = 1 + (objective_ab * bottom_modifier), ncp = 0, lower.tail = TRUE, log.p = FALSE) 
-               
-  
-#  top_left <- qbeta(p = c(0.01, 0.99),  shape1 = 1 + (objective_b * top_modifier), shape2 = 1 +(objective_a * top_modifier), ncp = 0, lower.tail = TRUE, log.p = FALSE)  # A B 
-  
-            
-#  top_right <- qbeta(p = c(0.01, 0.99) ,  shape1 = 1 + (objective_d * top_modifier), shape2 = 1 + (objective_c * top_modifier ), ncp = 0, lower.tail = TRUE, log.p = FALSE) # C D 
-  
-  
- 
- 
+  top_right <- hdi(qbeta(p = granularity ,  shape1 = 1 + (objective_d * top_modifier), shape2 = 1 + (objective_c * top_modifier ), ncp = 0, 
+                         lower.tail = TRUE, log.p = FALSE), credMass = HDI_width_per_cell, tol = 1e-5, allowSplit = TRUE ) # C D 
+
+
+
+ # Some manual calculation down here, and them we get our final intervals. 
+
  overall_a <- reflection(bottom)  * reflection(top_left)  # heads/left side is shape 1! 
  overall_b <- reflection(bottom)  * top_left
  overall_c <- bottom * reflection(top_right) 
  overall_d <- bottom * top_right 
  
- 
- #For the jiggle problem. Will only get worse when we add more cells. Probably, anyway... 
- # Not a bug? Maybe if only organized according to an ordinal scale... 
- 
- # Equal tailed posterior does not work... PROBABLY, ANYWAY 
- 
- 
- #.... WHAT ELSE IS THERE! 
- 
- 
- 
- 
-#------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-   
-
-print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
- # this is a test of only three outcomes. 
- 
- key <- 0.95**(1/2)
-
- objective_a <- 44
- objective_b <- 44
- objective_c <- 44 
- objective_d  <- -1   # only three outcomes 
- 
- 
- objective_bc <- objective_b + objective_c
- 
- bottom_modifier <- 0.5
- top_modifier <- 1         
- 
-# overall_a <- 0
-# overall_b <- 0 
-# overall_c <- 0 
-# overall_d <- -1 
- 
- 
-# bottom <- hdi(qbeta(p = x,  shape1 = 100, shape2 = 3, ncp = 0, lower.tail = TRUE, log.p = FALSE), 
- #               credMass = key, tol = 1e-5, allowSplit = TRUE ) 
- 
- 
-# top_right <- hdi(qbeta(p = x ,  shape1 = 1 + (objective_b * top_modifier), shape2 = 1 + (objective_c * top_modifier ), ncp = 0, lower.tail = TRUE, log.p = FALSE), 
-  #                credMass = key, tol = 1e-5, allowSplit = TRUE ) # B C 
- 
-# overall_a <- reflection(bottom) 
-# overall_b <- bottom  * top_right 
-# overall_c <- bottom * reflection(top_right) 
- 
-# print(overall_a[1])
-# print(overall_b[1])
-# print(overall_c[1])
-# print(overall_d[1])
+print('overall_a:', overall_a)
+print('overall_b:', overall_b)
+print('overall_c:', overall_c)
+print('overall_d:', overall_d)
 
 
-print(bottom)
-
  
-print(overall_a)
-print(overall_b)
-print(overall_c)
-print(overall_d)
 
    
